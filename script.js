a3794c197002d6f29c22825cfaa644388ecdea4a
needleworm <bhban@kakao.com>
2026-01-07 21:58:57 +0900

    .

--- script.js
+++ script.js
@@ -243,11 +243,12 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             card.innerHTML = `
-                ${screenshotHtml}
+            ${screenshotHtml}
                 <div class="prompt-text">${item.prompt}</div>
                 <div class="copy-icon-card">
                     복사하기 <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
                 </div>
+                ${item.comment ? `<div class="card-comment">${item.comment}</div>` : ''}
                 <div class="card-footer">${footerText}</div>
             `;
 
